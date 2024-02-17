0ebd191e76d5935dd8b308383ff7ff7794408fb7
jass <jasbrar080@gmail.com>
2024-02-17 14:19:02 -0800

    Understanding the effect hook

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react'
+import './App.css'
+
+function App() {
+  const ref=useRef<HTMLInputElement>(null);
+  /*useEffect(()=>{
+    if(ref.current) ref.current.focus();
+  });*/
+  useEffect(()=>{
+    document.title="My App"
+  });
+  return(
+  <div>
+  <input ref={ref} type="text" className="form-control" />
+  </div>);
+}
+
+export default App
