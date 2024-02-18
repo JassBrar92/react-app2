43803755efce60d3f0dbc128d2ff173030e81595
jass <jasbrar080@gmail.com>
2024-02-18 12:09:03 -0800

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import './ProductList'
 import ProductList from './ProductList'
-
+import axios from 'axios'
+interface User{
+  id:number,
+  name:string
+ }
 function App() {
+  const [users,setUsers]=useState<User[]>([]);
+  useEffect(()=>{
+    axios.get<User[]>("https://jsonplaceholder.typicode.com/users").
+    then((res)=>setUsers(res.data));
+  },[]);
+  return <ul>
+   {users.map(user=><li key={user.id}>{user.name}</li>)}
+  </ul>
+
+  /*
   const connect=()=>console.log("Connecting");
   const disconnect=()=>console.log("Disconnecting");
   useEffect(()=>{
     connect();
     return ()=>disconnect();
-  })
+  })*/
  /* const [category,setCategory]=useState("");
 
 return(
