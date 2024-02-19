2493e24197c58355b66ed7ced113827fb2cb38ce
jass <jasbrar080@gmail.com>
2024-02-19 12:19:05 -0800

    creating data

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,21 @@ function App() {
     });
     return ()=>controller.abort();
   },[]);
+  const addUser=()=>{
+    const originalUsers=[...users];
+    const newUser={id:0,name:"jas"};
+    setUsers([newUser,...users]);
+    axios.post("https://jsonplaceholder.typicode.com/users",newUser).
+    then(res=>setUsers([res.data,...users])).
+    catch(err=>{
+      setError(err.message);
+      setUsers(originalUsers);
+    })
+  }
   const deleteUser=(user:User)=>{
     const originalUsers=[...users];
  setUsers(users.filter(u => u.id !== user.id));
- axios.delete("https://jsonplaceholder.typicode.com/xusers/"+user.id).
+ axios.delete("https://jsonplaceholder.typicode.com/users/"+user.id).
  catch(err=>{
  setError(err.message);
  setUsers(originalUsers);
@@ -40,6 +51,7 @@ function App() {
    <>
    {error && <p className='text-danger'>{error}</p>}
    {isLoading && <div className="spinner-border"></div>}
+   <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
    <ul className='list-group'>
    {users.map(user=>(<li key={user.id} className='list-group-item d-flex justify-content-between'>
     {user.name}
